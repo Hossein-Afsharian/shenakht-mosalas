74cf73ce555ca30ac1dfca4e1b51b513b29efd6d
Hossein-Afsharian <148617504+Hossein-Afsharian@users.noreply.github.com>
2023-10-21 15:44:45 +0330

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,9 @@
+print("andaze azlae mosalas ra benevisid")
+a=int(input("inter A:"))
+b=int(input("inter B:"))
+c=int(input("inter C:"))
+
+if a+b>c and a+c>b and b+c>a
+    print("mosalas hast")
+else:
+    print("mosalas nist")
